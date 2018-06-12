08e90ef5d7f44200f1e9e1609325d69d4174a433
kansalaman <amankansal.india@gmail.com>
2018-06-12 16:09:06 +0530

    trivial change

--- getkey.py
+++ getkey.py
@@ -1,23 +1,29 @@
 import http.client,urllib.request,urllib.parse,urllib.error,json, base64
 headers={
 'Content-Type':'application/json',
-'Ocp-Apim-Subscription-Key':'d80ae700fdba4e839e7b209bb82ec100'
+'Ocp-Apim-Subscription-Key':''
 }
 params=urllib.parse.urlencode({})
 conn=http.client.HTTPSConnection('southeastasia.api.cognitive.microsoft.com')
 fhand=open('demotext.txt')
+lst_of_words=[]
 for line in fhand:
     if len(line)<1:
         break
-    rbody='''{
+    rbody={
     'documents':[{
     'language':'en',
     'id':'1',
     'text':line
-    }]}'''
-    conn.request("POST","/text/analytics/v2.0/keyPhrases?%s" % params,rbody,headers)
+    }]}
+    conn.request("POST","/text/analytics/v2.0/keyPhrases?%s" % params,json.dumps(rbody),headers)
     response=conn.getresponse()
     data=response.read().decode()
     resultdic=json.loads(data)
     print(resultdic)
+    if(resultdic['errors']==[]):
+        lst_of_words.extend(resultdic['documents'][0]['keyPhrases'])
+    else:
+        continue
+print(lst_of_words)
 conn.close()
